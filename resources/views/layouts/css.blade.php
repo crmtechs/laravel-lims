6bca4472867d829260fb4d47453fdaa2be6f2311
crmtechs <info@crmtechs.com>
2026-06-13 11:23:04 +0530

    Sweetalert2 added to project

--- resources/views/layouts/css.blade.php
+++ resources/views/layouts/css.blade.php
@@ -1,12 +1,14 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="stylesheet" href="{{ asset('css/source-sans-3.css') }}">
+<link rel="stylesheet" href="{{ asset('css/source-sans-3.css') }}?v={{ filemtime(public_path('css/source-sans-3.css')) }}">
 
-<link rel="stylesheet" href="{{ asset('plugins/overlayscrollbars/overlayscrollbars.min.css') }}">
+<link rel="stylesheet" href="{{ asset('plugins/overlayscrollbars/overlayscrollbars.min.css') }}?v={{ filemtime(public_path('plugins/overlayscrollbars/overlayscrollbars.min.css')) }}">
 
-<link rel="stylesheet" href="{{ asset('plugins/bootstrap-icons/bootstrap-icons.min.css') }}">
+<link rel="stylesheet" href="{{ asset('plugins/sweetalert2/sweetalert2.min.css') }}?v={{ filemtime(public_path('plugins/sweetalert2/sweetalert2.min.css')) }}">
 
-<link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}">
+<link rel="stylesheet" href="{{ asset('plugins/bootstrap-icons/bootstrap-icons.min.css') }}?v={{ filemtime(public_path('plugins/bootstrap-icons/bootstrap-icons.min.css')) }}">
 
-<link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+<link rel="stylesheet" href="{{ asset('css/adminlte.min.css') }}?v={{ filemtime(public_path('css/adminlte.min.css')) }}">
+
+<link rel="stylesheet" href="{{ asset('css/custom.css') }}?v={{ filemtime(public_path('css/custom.css')) }}">
